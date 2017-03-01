09d8ba825fce0fdb82107516a6abf7717c342801
pshastri <praveen.shastri86@gmail.com>
2017-03-01 17:16:02 +0530

    changes today

--- FindingHostName.py
+++ FindingHostName.py
@@ -1,7 +1,14 @@
 import os
 import socket
 
-myhost=socket.gethostname()
-myipaddr=socket.gethostbyname(myhost)
-print myhost, "\n", myipaddr
+myhost=raw_input()
+getmyhost=socket.gethostname()
+getmyipaddr=socket.gethostbyname(getmyhost)
+getmyfqdn=socket.getfqdn()
+#mydomainname=
+#getmymyhost=socket.gethostname(myhost)
+getmymyipaddr=socket.gethostbyname(myhost)
+getmymyfqdn=socket.getfqdn(myhost)
+print getmyhost, "\n", getmyipaddr,"\n",getmyfqdn,"\n"
+print "\n",getmymyipaddr,"\n",getmymyfqdn
 
