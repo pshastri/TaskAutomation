72254b36784c6e464ada767aefbe049063258be6
pshastri <praveen.shastri86@gmail.com>
2017-03-26 18:59:24 +0530

    list with symbol n number

--- src/evaluateExpression.py
+++ src/evaluateExpression.py
@@ -4,8 +4,10 @@ Created on Mar 21, 2017
 @author: pshastri
 '''
 def parseExpression(inputExpression):
-    allDigitExpression=['']
-    allSymbolExpression=['']
+    answer=0
+    allDigitExpression=[""]
+    newExpressionList=[]
+#     allSymbolExpression=[]
     for eachChar in inputExpression:
         if eachChar.isdigit():
             if allDigitExpression[-1].isdigit():
@@ -13,6 +15,20 @@ def parseExpression(inputExpression):
             else:
                 allDigitExpression.append(eachChar)
         else:
-            allSymbolExpression.append(eachChar)
+            allDigitExpression.append(eachChar)
+    allDigitExpression.remove('')
     print allDigitExpression
-    print allSymbolExpression
+#     print type(allDigitExpression[0])
+#     for i in range (len(allDigitExpression)):
+#         if allDigitExpression[i].isdigit():
+#             allDigitExpression[i]=int(allDigitExpression[i])
+#         else:
+#             allDigitExpression[i]=allDigitExpression[i]
+#     print allSymbolExpression
+#     print inputExpression.index("+" or "-")
+#     for iterI in range (len(allDigitExpression)-1):
+#         if 
+    
+
+if __name__=="__main__" :
+    parseExpression("989+9")
