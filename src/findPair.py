5b0f125edf86f373ec20775518923c28d070457e
pshastri <praveen.shastri86@gmail.com>
2017-04-08 15:47:04 +0530

    formatted output

--- src/findPair.py
+++ src/findPair.py
@@ -9,21 +9,26 @@ def hasSeenSum(myinput,testsum):
     myCompList=set()
     
     for i in myinput:
-        print "myinput: ", myinput
-        print "comList: ", myCompList
-        print "i: ", i
+#         print "myinput: ", myinput
+#         print "comList: ", myCompList
+#         print "i: ", i
         mycomp=testsum-i
         if i in myCompList:
-            print "sum-i: ", mycomp
-            print "Found"
+#             print "sum-i: ", mycomp
+            print "Found the pair"
+            print "pair: %d and %d" %(i,mycomp)
+            return True
+            
+            break
         else: 
             myCompList.add(mycomp)
-            print "Not found"
+            mybool=False
     
-        
+    if not mybool:
+        print "Did not find the sum or pair"   
     
     
 if __name__=="__main__":
     myinput=[1,2,3,4]
-    testsum=7
+    testsum=10
     hasSeenSum(myinput, testsum)
